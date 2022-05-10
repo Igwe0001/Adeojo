219ad8b9442f6bc9e9bd893bc6a931be2e47e438
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-05-10 23:57:36 +0100

    new changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
 const hamburger = document.querySelector('.hamburger');
+const lines = document.querySelectorAll('.line')
 const close = document.querySelector('.close');
+const bun = document.querySelectorAll('.bun');
+const bunCrust = document.querySelectorAll('.bun-crust')
 
 hamburger.addEventListener('click', () => {
-    nav.style.width = '40%';
-    body.style.marginRight = '40%';
-    hamburger.style.display = 'none';
-})
-
-close.addEventListener('click', () => {
-     nav.style.width = '0';
-    body.style.marginRight = '0';
-    hamburger.style.display = 'block';
+    nav.classList.toggle('switch')
+    bun.forEach(e => {
+        e.classList.toggle('toggle')
+    })
+    bunCrust.forEach(e => {
+        e.classList.toggle('toggle')
+    })
 })
